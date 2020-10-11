2346e957024d779a59857f011c092be1e3aa4e68
DaKo <gitlabuser2@subseq.net>
2020-10-11 19:06:46 +0200

    + composePlus: combines the output of multiple TwoTrackOutputs

--- src/main/kotlin/net/lazy_bit/kotlin_train/tracks/adapters.kt
+++ src/main/kotlin/net/lazy_bit/kotlin_train/tracks/adapters.kt
@@ -90,6 +90,36 @@ fun <IN, FAILURE, OUT, NEW_FAILURE, NEW_OUT>TwoTrackOutput<IN, FAILURE, OUT>.map
 }
 
 /**
+ * Evaluates [other] with [moreOthers] in parallel with [this] as input and combines their results according
+ * to [addSuccess] and [addFailure] if multiple fail or all succeed.
+ */
+fun <IN, FAILURE, OUT, NEW_OUT>TwoTrackOutput<IN, FAILURE, OUT>.composePlus(
+    addSuccess: (NEW_OUT, NEW_OUT) -> NEW_OUT,
+    addFailure: (FAILURE, FAILURE) -> FAILURE,
+    other: SwitchFunction<OUT, FAILURE, NEW_OUT>,
+    vararg moreOthers: SwitchFunction<OUT, FAILURE, NEW_OUT>
+) : TwoTrackOutput<IN, FAILURE, NEW_OUT> = {
+    when (val res = this(it)) {
+        // skip if on failure track
+        is Either.Left -> left(res.value)
+        // evaluate on success track
+        is Either.Right -> moreOthers.fold( other(res.value) ) { acc, function ->
+            // for every combined track
+            val b = function(res.value)
+            // combine their results
+            when {
+                acc is Either.Right && b is Either.Right -> right(addSuccess(acc.value, b.value))
+                acc is Either.Right && b is Either.Left -> left(b.value)
+                acc is Either.Left && b is Either.Right -> left(acc.value)
+                acc is Either.Left && b is Either.Left -> left(addFailure(acc.value, b.value))
+                else -> error("Can not happen!!!")
+            }
+        }
+    }
+}
+
+
+/**
  * Turn a value into a [SwitchFunction] on the success track
  */
 fun <IN> success(value: IN): Success<IN> = { right(value) }
@@ -106,7 +136,7 @@ operator fun <T>((Unit)->T).invoke(): T = this(Unit)
 
 
 fun main() {
-    val x = success("4s") composeSwitch
+    val x = success("4") composeSwitch
             {x: String -> x.toInt()}.tryCatch() composeSwitch
             { x: Int -> x*x }.switch()
 
@@ -118,5 +148,26 @@ fun main() {
             l = {it},
             r = {it}
         )
+    )
+
+    val y =  x.mapBoth(
+        success = {it},
+        failure = {"Failed with `${it.message}`"}
+    ).mapBoth(
+        success = {it},
+        failure = {listOf(it)}
+    ).composePlus(
+        addSuccess = {a, b -> a + b},
+        addFailure = {a, b -> (a) + (b)},
+        { right(it) },
+        { right(2) },
+        { right(-it) },
+        { left(listOf("failure 1")) },
+        { left(listOf("failure 2")) },
+        { left(listOf("failure 3")) }
+    )
+
+    println(
+       y()
     )
 }
